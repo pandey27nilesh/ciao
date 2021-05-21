c465995f23dc13f44e1fa2c6284a36cee2c491fd
@ditya <pandey27nilesh@gmail.com>
2021-05-22 02:55:32 +0530

    dfh

---
+++ src/Modal.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {props.initial_state ? (
+        <div id="login-box">
+          <div class="left">
+            <h1>Sign up</h1>
+
+            <input type="text" name="username" placeholder="Username" />
+            <input type="text" name="email" placeholder="E-mail" />
+            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password2"
+              placeholder="Retype password"
+            />
+
+            <input type="submit" name="signup_submit" value="Sign me up" />
+          </div>
+
+          <div class="right">
+            <span class="loginwith">
+              Sign in with
+              <br />
+              social network
+            </span>
+
+            <button class="social-signin facebook">Log in with facebook</button>
+            <button class="social-signin twitter">Log in with Twitter</button>
+            <button class="social-signin google">Log in with Google+</button>
+          </div>
+          <div class="or">OR</div>
+        </div>
+      ) : null}
+
+      {props.final_state ? null : null}
+    </React.Fragment>
+  );
+};
+
+export default Modal;
